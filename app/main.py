7578fed6c72419e25c69f083c4c801f33a8d5ba7
Joseph Hamman <jhamman@ucar.edu>
2020-04-16 10:02:24 -0700

    add projects route

--- app/main.py
+++ app/main.py
@@ -1,20 +1,42 @@
+from functools import lru_cache
+
+import json
+
 from fastapi import FastAPI, Request
 
 
 app = FastAPI()
 
+
+@lru_cache
+def get_data(kind):
+    if kind == 'projects':
+        with open('projects.json', 'r') as f:
+            return json.load(f)
+    else:
+        raise NotImplementedError(kind)
+
+
 # Return a Cache-Control header for all requests.
 # The no-cache directive disables caching on the zeit CDN.
 # Including this better demonstrates using FastAPI as a
 # serverless function.
-@app.middleware("http")
+@app.middleware('http')
 async def add_no_cache_header(request: Request, call_next):
     response = await call_next(request)
-    response.headers["Cache-Control"] = "no-cache"
+    response.headers['Cache-Control'] = 'no-cache'
     return response
 
 
-@app.get('/')
-@app.get('/hello')
-def hello():
-    return {'hello': 'world!!'}
+@app.get('/projects')
+def projects(id: str = None):
+    data = get_data('projects')
+
+    if id is None:
+        return data
+
+    for p in data['features']:
+        if p['project_id'] == id:
+            return p
+
+    return {}
