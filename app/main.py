21eeb9c9cc4442dca7dceb326e9cdfdd3b0302dd
Joseph Hamman <jhamman@ucar.edu>
2020-04-16 09:33:21 -0700

    Cache-Control

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 
 app = FastAPI()
+
+# Return a Cache-Control header for all requests.
+# The no-cache directive disables caching on the zeit CDN.
+# Including this better demonstrates using FastAPI as a
+# serverless function.
+@app.middleware("http")
+async def add_no_cache_header(request: Request, call_next):
+    response = await call_next(request)
+    response.headers["Cache-Control"] = "no-cache"
+    return response
 
 
 @app.get('/')
