bda84741ed16b858c1b3a9bec296385ac59d1b10
freeman-lab <the.freeman.lab@gmail.com>
2020-04-25 17:09:51 -0700

    add project

--- munge.py
+++ munge.py
@@ -77,7 +77,7 @@ def maybe_float(value):
 
 if __name__ == '__main__':
 
-    data = get_sheet('Sheet1', 'Stripe reports 0.1 [internal]').loc[:3]
+    data = get_sheet('Sheet1', 'Stripe reports 0.1 [internal]').loc[:6]
 
     metrics = [
         'mechanism',
